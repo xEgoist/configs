74084e4c48d4f1fad1167b34668afca06f30db3f
xEgoist <egoist@egoistic.dev>
2023-06-28 12:20:39 -0500

    Improved checking for wpctl at boot time

--- .config/waybar/scripts/wpctl.sh
+++ .config/waybar/scripts/wpctl.sh
@@ -1,13 +1,12 @@
 #! /bin/sh
 
-# BUGFIX: Since Wireplumber doesn't wait at boot for initialization, we need to wait manually here.
-# Maybe it will be fixed in the future.
-UPTIME=$(awk '{print int($1)}' /proc/uptime)
-if [ $UPTIME -lt 30 ]
-then
-  sleep 1
-fi 
-TEXT=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | grep -Eo "MUTED|[0-9]\.[0-9]*" | tail -1 | awk '{print($1 == "MUTED" ? "🔇" : $1 * 100"% 🔊")}')
-TOOLTIP=$(wpctl inspect @DEFAULT_AUDIO_SINK@ | grep  "node.nick" | cut -d "=" -f2 | tr -d "\"")
+TEXT=""
+while [[ -z "$TEXT"  ]]; do
+  TEXT=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | grep -Eo "MUTED|[0-9]\.[0-9]*" | tail -1 | awk '{print($1 == "MUTED" ? "🔇" : $1 * 100"% 🔊")}')
+  TOOLTIP=$(wpctl inspect @DEFAULT_AUDIO_SINK@ | grep  "node.nick" | cut -d "=" -f2 | tr -d "\"")
+  # BUGFIX: Since Wireplumber doesn't wait at boot for initialization, we need to wait manually here.
+  # Maybe it will be fixed in the future.
+done
+
 
 printf "%s\nDevice:%s\n\n" "${TEXT}" "${TOOLTIP}"
