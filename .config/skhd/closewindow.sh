c31901d5f6f3bec01413fd86c0e116a873ff4014
QuincePie <flaky@quincepie.dev>
2024-08-20 18:45:15 -0500

    fallback to any current window when no window is focused

--- .config/skhd/closewindow.sh
+++ .config/skhd/closewindow.sh
@@ -5,3 +5,14 @@ NEW="$(yabai -m query --spaces --space | "${jq}" '.index')"
 if ! [ "$OLD" = "$NEW" ]
   then yabai -m space --focus "$OLD"
 fi
+
+if ! $(yabai -m query --windows --window); then
+    # If no window is focused, focus on any window in the current space, this prevents something like safari from stealing the focus
+    SPACE_WINDOWS=$(yabai -m query --windows --space)
+    FIRST_WINDOW=$(echo "$SPACE_WINDOWS" | jq '.[0].id')
+
+    if [ "$FIRST_WINDOW" != "null" ]; then
+        yabai -m window --focus "$FIRST_WINDOW"
+    fi
+
+fi
